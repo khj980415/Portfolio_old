d4fb3589f8d083feea4c67be9fe04d6829b46c0c
khj980415 <khj980415@naver.com>
2024-02-26 16:13:59 +0900

    2024-02-26/1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,94 @@
 window.addEventListener("load", () => {
+	let cursor=document.querySelector(".mouse_cursor");
+
+	let header=document.getElementById("header");
+	let nav=header.lastElementChild;
+	let navLi=nav.firstElementChild.children;
+	let pageList=[];
+	let section=document.querySelectorAll("section");
+	let footer=document.getElementById("footer");
+	let targety=0;
+
+	for(let i=0; i<section.length; i++){
+		pageList.push(section[i]);
+	}
+	pageList.push(footer);
+	
+
+	document.body.addEventListener("mousemove", function(e){
+		gsap.to(cursor, {left: e.pageX-5, top: e.pageY-5, duration: 0.2})
+	});
+
+	const trigger=new ScrollTrigger.default({
+		trigger: { //  추가 클래스와 반응 위치
+				once: true,
+				offset: {
+					viewport: {
+						x: 0,
+						y: 0.25 // 윈도우 3/4 지점에서 발생
+					}
+				},
+				toggle: {
+					class: {
+						in: "active",
+						out: "inactive"
+					}
+				},
+			},
+		scroll: { // 페이지 번호를 JavaScript로 받기 위한 
+					callback: offset => scrollInteraction(offset.y)
+				}
+		});
+	
+	trigger.add("section, footer");
+
+	let n=0;
+	
+	function scrollInteraction(t){
+		if(t<pageList[1].offsetTop){
+			n=0;
+		}
+		else if (t < pageList[2].offsetTop){
+			n=1;
+		}
+		else if (t < pageList[3].offsetTop){
+			n=2;
+		}
+		else if (t < pageList[4].offsetTop){
+			n=3;
+			if( window.innerHeight+t === document.body.scrollHeight){
+				n=4;
+			}
+		}
+		else {
+			n=4;
+		}
+
+		for(let i=0; i<navLi.length; i++){
+			if(i===n){
+				if(!navLi[i].firstElementChild.classList.contains("active")){
+					console.log(n);
+					navLi[i].firstElementChild.classList.add("active");
+				}
+			}
+			else{
+				if(navLi[i].firstElementChild.classList.contains("active")){
+					navLi[i].firstElementChild.classList.remove("active");
+				}
+			}
+		}
+		
+	}
+	
+	for(let i=0; i<navLi.length; i++){
+		navLi[i].addEventListener("click", function(e){
+			e.preventDefault();
+			targety=pageList[i].offsetTop;
+			gsap.to(window, {scrollTo: targety, duration: 0.5});
+		});
+	}
+	
+
 	let marqueeTop=document.querySelector("#sec4 .title_area .top");
 	let marqueeTopText=marqueeTop.firstElementChild;
 	
@@ -42,15 +132,31 @@ window.addEventListener("load", () => {
 		}
 	}
 
+	function cursorDisplay(){
+	if (device === "mobile"){
+		if(!document.body.classList.contains("mobile")){
+		cursor.style.display="none";
+		this.document.body.classList.add("mobile");
+		}
+	}
+	else{
+		if(document.body.classList.contains("mobile")){
+		cursor.style.display="block";
+		this.document.body.classList.remove("mobile");
+		}
+	}
+}
+
+	
 	marqueeSize();
-	window.addEventListener("resize", marqueeSize);
-
-	let cursor=document.querySelector(".mouse_cursor");
-
-	document.body.addEventListener("mousemove", function(e){
-		gsap.to(cursor, {left: e.pageX-5, top: e.pageY-5, duration: 0.2})
+	cursorDisplay();
+	
+	window.addEventListener("resize", function(){
+		marqueeSize();
+		cursorDisplay();
 	});
 
+	
 	let projectLink=marqueeTop.parentElement;
 
 	projectLink.addEventListener("mouseenter", function(){
@@ -59,25 +165,4 @@ window.addEventListener("load", () => {
 	projectLink.addEventListener("mouseleave", function(){
 		cursor.classList.remove("active");
 	});
-
-	let header=document.getElementById("header");
-	let nav=header.lastElementChild;
-	let navLi=nav.firstElementChild.children;
-	let pageList=[];
-	let section=document.querySelectorAll("section");
-	let footer=document.getElementById("footer");
-	let targety=0;
-
-	for(let i=0; i<section.length; i++){
-		pageList.push(section[i]);
-	}
-	pageList.push(footer);
-	
-	for(let i=0; i<navLi.length; i++){
-		navLi[i].addEventListener("click", function(e){
-			e.preventDefault();
-			targety=pageList[i].offsetTop;
-			gsap.to(window, {scrollTo: targety, duration: 0.5});
-		});
-	}
 });
